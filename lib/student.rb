f09b8c70d715425a0bd45ec7820826b366de36ac
Amar Chatman <amarjerome@gmail.com>
2019-11-16 00:46:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -61,7 +61,7 @@ def self.find_by_name(name)
     DB[:conn].execute(sql)
 end
 def self.find_by(attribute)
-  binding.pry
+  #binding.pry
   sql = "SELECT * FROM #{self.table_name} WHERE name = '#{attribute.keys}'"
     DB[:conn].execute(sql)
 end
