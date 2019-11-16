119254e2a9b7113cb5e5613db977dd2c742a061e
Amar Chatman <amarjerome@gmail.com>
2019-11-16 01:26:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -64,9 +64,9 @@ def self.find_by(attribute)
   #binding.pry
   if attribute.values[0].class == Integer
     value = attribute.values[0]
-  else attribute = "'#{attribute.values[0]}'"
+  else value = "'#{attribute.values[0]}'"
   end
-  sql = "SELECT * FROM #{self.table_name} WHERE '#{attribute.keys[0].to_s}' = #{attribute}"
+  sql = "SELECT * FROM #{self.table_name} WHERE '#{attribute.keys[0].to_s}' = #{value}"
     DB[:conn].execute(sql)
 end
 
